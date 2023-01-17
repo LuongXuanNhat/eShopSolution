996af2f48a8c9c62fd3b7d263648ed5535a56cee
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-01-17 17:05:14 +0700

    Update EF Core, Fluent API, Migration

---
+++ eShopSolution.Data/Configurations/ContactConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ContactConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("Contacts");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Email).IsRequired().IsUnicode(false).HasMaxLength(50);
+            builder.Property(x => x.PhoneNumber).HasMaxLength(15).IsRequired();
+            builder.Property(x => x.Message).IsRequired();
+        }
+    }
+}
